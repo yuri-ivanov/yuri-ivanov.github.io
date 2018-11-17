bb870701f5bdb388b0c588bf1ed3f4a4c8f7bd13
Yuri Ivanov <yura.ivanov@gmail.com>
2018-11-17 21:19:21 +0100

    remote gatsby from master

--- gatsby/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/yivanov/yuri-ivanov.github.io/gatsby/node_modules/gatsby-plugin-emotion/gatsby-browser'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/home/yivanov/yuri-ivanov.github.io/gatsby/node_modules/gatsby-plugin-offline/gatsby-browser'),
-      options: {"plugins":[]},
-    }]
--- gatsby/src/util/typography.js
+++
@@ -1,6 +0,0 @@
-import Typography from "typography"
-import fairyGateTheme from "typography-theme-fairy-gates"
-
-const typography = new Typography(fairyGateTheme)
-export const rhythm = typography.rhythm
-export default typography
