2d0ba307a3181e82f1f8445af805619c290f1e48
Yuri Ivanov <yura.ivanov@gmail.com>
2018-11-19 22:39:36 +0100

    add bootstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
+import './bootstrap.min.css';
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -36,7 +37,7 @@ const Layout = ({ children }) => (
             padding: '0px 1.0875rem 1.45rem',
             paddingTop: 0,
           }}
-        >
+        >children
           {children}
         </div>
       </>
