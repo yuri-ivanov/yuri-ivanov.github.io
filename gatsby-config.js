99d0612b7a87a257cad1ec3ab3900782c12e726f
Yuri Ivanov <yura.ivanov@gmail.com>
2019-01-19 12:30:34 +0100

    update gatsby portfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,14 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    'gatsby-transformer-json',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/data`,
+        typeName: 'Json'
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,17 +3,37 @@ import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const PortfolioPage = () => (
-  <Layout>
-    <h1>Portfolio Page</h1>
-    <section className="work">
-      <header>Capgemini Sweden<time>2018</time></header>
-      <article>
-        <header>Project Name</header>
-      </article>
-    </section>
-    
-  </Layout>
-)
 
+class PortfolioPage extends React.Component {
+  
+  render() {
+    console.log('state', this.props);
+    let data = '';
+    let projects = this.props.data.allProjectsJson.edges;
+    console.log('projects', projects);
+    return (
+      <Layout>
+        <h1>Portfolio Page 2</h1>
+        <section className="work">
+          <header>Capgemini Sweden<time>2018</time></header>
+          <article>
+            <header>Project Name</header>
+          </article>
+        </section>
+        {projects.map(({node})=>( <div>{node.projectName}</div> ))}
+      </Layout>
+    );
+  }
+}
+export const query = graphql`
+  query {
+    allProjectsJson{
+      edges{
+        node{
+          projectName
+        }
+      }
+    }
+  }
+`
 export default PortfolioPage
